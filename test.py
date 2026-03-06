5c174ac609ecafd54a94424f50ceeeec49d72deb
Okeke Chiemerie <113051226+Charlysmart@users.noreply.github.com>
2026-03-06 04:27:35 +0100

    Update test.py

--- test.py
+++ test.py
@@ -7,8 +7,8 @@ app = FastAPI()
 def root():
     return {"message": "API is running", "PORT": os.getenv("PORT")}
 
-# Use platform-assigned port
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", 5000))
+    # Must use platform PORT
+    port = int(os.environ["PORT"])
     uvicorn.run("main:app", host="0.0.0.0", port=port, log_level="info")
