0c4936842355e9cb57a03ce63e6f196336608a4c
Okeke Chiemerie <charleschiemerie2004@gmail.com>
2026-03-19 23:57:57 +0100

    Disabled prepared statement

--- database/connect.py
+++ database/connect.py
@@ -4,7 +4,9 @@ from sqlalchemy.orm import declarative_base
 from config.setting import Setting
 
 setting = Setting()
-engine = create_async_engine(url=setting.databaseurl, echo=True)
+engine = create_async_engine(url=setting.databaseurl, echo=True, connect_args={
+        "statement_cache_size": 0  # 🔥 THIS FIXES IT
+    })
 
 SessionLocal = async_sessionmaker(
     class_= AsyncSession,
